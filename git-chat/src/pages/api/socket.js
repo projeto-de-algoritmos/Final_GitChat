47ff602e744e1ae10cfa15f332075360bad1b243
luucas-melo <lucasmelodos322@gmail.com>
2022-04-23 12:56:41 -0300

    Add socket connection

---
+++ git-chat/src/pages/api/socket.js
@@ -0,0 +1,22 @@
+import { Server as ServerIO } from 'socket.io';
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req, res) => {
+  if (!res.socket.server.io) {
+    console.log('New Socket.io server...');
+    // adapt Next's net Server to http Server
+    const httpServer = res.socket.server;
+    const io = new ServerIO(httpServer, {
+      path: '/api/socket',
+    });
+    // append SocketIO server to Next.js socket server response
+    res.socket.server.io = io;
+  }
+  res.end();
+};
