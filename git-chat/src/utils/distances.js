f9aee454165a3a7fb4b468e650e07b6e37335a7d
luucas-melo <lucasmelodos322@gmail.com>
2022-04-24 18:05:12 -0300

    Add dijkstra algorithm
    Co-authored-by: Antonio Neto <netocaastro10@gmail.com>

---
+++ git-chat/src/utils/distances.js
@@ -0,0 +1,59 @@
+export const edges = {
+  //NORTE
+  'Manaus:Boa Vista': 785,
+  'Manaus:Belem': 5298,
+
+  'Porto Velho:Manaus': 901,
+  'Rio Branco:Manaus': 1445,
+  'Rio Branco:Porto Velho': 544,
+
+  //NORDESTE
+  'Teresina:Sao Luis': 446,
+  'Teresina:Fortaleza': 634,
+  'Teresina:Recife': 1137,
+  'Natal:Joao Pessoa': 185,
+  'Natal:Fortaleza': 537,
+  'Recife:Joao Pessoa': 118,
+  'Joao Pessoa:Fortaleza': 688,
+  'Recife:Maceio': 285,
+  'Salvador:Recife': 839,
+  'Maceio:Aracaju': 294,
+  'Salvador:Maceio': 632,
+  'Salvador:Aracaju': 356,
+
+  //CENTRO-OESTE
+  'Cuiaba:Campo Grande': 694,
+  'Goiania:Cuiaba': 934,
+  'Goiania:Brasilia': 209,
+  'Goiania:Campo Grande': 935,
+
+  //SUDESTE
+  'Sao Paulo:Rio de Janeiro': 429,
+  'Rio de Janeiro:Belo Horizonte': 434,
+  'Sao Paulo:Belo Horizonte': 586,
+  'Vitoria:Rio de Janeiro': 521,
+  'Vitoria:Belo Horizonte': 524,
+
+  //SUL
+  'Florianopolis:Curitiba': 300,
+  'Porto Alegre:Florianopolis': 476,
+
+  //ARESTAS ENTRE REGIÕES
+  'Porto Velho:Cuiaba': 1456,
+  'Manaus:Cuiaba': 2357,
+  'Cuiaba:Belem': 2941,
+  'Palmas:Cuiaba': 1784,
+  'Palmas:Goiania': 874,
+  'Sao Luis:Belem': 806,
+  'Sao Luis:Palmas': 1386,
+  'Salvador:Palmas': 1454,
+  'Curitiba:Campo Grande': 991,
+  'Salvador:Goiania': 1643,
+  'Sao Paulo:Curitiba': 408,
+  'Salvador:Belo Horizonte': 1372,
+  'Vitoria:Salvador': 1202,
+  'Goiania:Belo Horizonte': 906,
+  'Brasilia:Belo Horizonte': 716,
+  'Campo Grande:Belo Horizonte': 1453,
+  'Sao Paulo:Campo Grande': 1014,
+};
